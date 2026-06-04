7cd2978312f2e4b9874528abdc6c1b235c72278a
Yuri Teixeira <yuriteixeirac@proton.me>
2026-06-04 13:30:24 -0300

    feat: projetos section

--- src/components/ProjetoCard.tsx
+++ src/components/ProjetoCard.tsx
@@ -1,23 +1,45 @@
+import { CSSProperties } from "react";
 import Botao from "./Botao";
 
-type ProjetoProps = {
+type ProjetoCardProps = {
   nome: string;
   descricao: string;
   tecnologias: string[];
   demonstracao: string;
+  background: string;
 };
 
-export default function ProjetoCard(props: ProjetoProps) {
+export default function ProjetoCard(props: ProjetoCardProps) {
   return (
-    <div className="border-2 shadow-(--md-box-shadow) p-4">
-      <h2 className="text-2xl font-semibold">{props.nome}</h2>
-      <p>{props.descricao}</p>
+    <div
+      className="flex flex-col justify-between border-2 shadow-(--md-box-shadow) p-4 gap-2 max-w-xs"
+      style={
+        {
+          backgroundColor: props.background,
+        } as CSSProperties
+      }
+    >
+      <div>
+        <h2 className="text-2xl font-semibold border-b-2 mb-2">{props.nome}</h2>
+        <p className="leading-relaxed break-word hyphens-none">
+          {props.descricao}
+        </p>
+      </div>
 
-      <p id="tecnologias">
-        {props.tecnologias.map((tech) => {
-          return <span>{tech} </span>;
-        })}
-      </p>
+      <div className="flex flex-col gap-3">
+        <div className="flex flex-wrap gap-x-2 gap-y-1 mt-4 items-end justify-center">
+          {props.tecnologias.map((tech) => (
+            <span
+              key={tech}
+              className="bg-white border-2 border-black px-2 py-1 text-sm font-bold shadow-(--sm-box-shadow)"
+            >
+              {tech}
+            </span>
+          ))}
+        </div>
+
+        <Botao content="Ver código" href={props.demonstracao} color="#FF4F00" />
+      </div>
     </div>
   );
 }
--- src/components/Projetos.tsx
+++ src/components/Projetos.tsx
@@ -1,28 +1,33 @@
 import ProjetoCard from "./ProjetoCard";
+import { projetos } from "../../data/projetos";
 
 export default function Projetos() {
   return (
-    <div className="flex gap-16 m-auto justify-center mb-96">
-      <ProjetoCard
-        nome="notIFica"
-        descricao="lorem ipsum dolor sit amet"
-        tecnologias={["Python", "Django", "MySQL"]}
-        demonstracao="https://github.com/yuriteixeirac/notifica"
-      />
+    <div className="flex gap-8 justify-center mb-96">
+      <div className="mx-auto w-fit">
+        <h2 className="text-2xl font-bold bg-(--coral-pink) px-3 py-2 shadow-(--sm-box-shadow) border-2 inline-block p-1 mb-2">
+          Projetos
+        </h2>
+        <p className="max-w-5xl text-lg mb-3">
+          Projetos desenvolvidos para resolver problemas reais com aplicações
+          web, automações, APIs e ferramentas internas, todos publicados e
+          documentados no GitHub para pronto uso.
+        </p>
 
-      <ProjetoCard
-        nome="Laika"
-        descricao="lorem ipsum dolor sit amet"
-        tecnologias={["Python", "Django", "MySQL"]}
-        demonstracao="https://github.com/yuriteixeirac/notifica"
-      />
-
-      <ProjetoCard
-        nome="notIFica"
-        descricao="lorem ipsum dolor sit amet"
-        tecnologias={["Python", "Django", "MySQL"]}
-        demonstracao="https://github.com/yuriteixeirac/notifica"
-      />
+        <div className="flex gap-8">
+          {projetos.map((data) => {
+            return (
+              <ProjetoCard
+                nome={data.nome}
+                descricao={data.descricao}
+                tecnologias={data.tecnologias}
+                demonstracao={data.demonstracao}
+                background={data.background}
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
